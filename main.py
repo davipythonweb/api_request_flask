9ac81102d1c796d5db2976e7eed17af6ee9bf4cd
davipythonweb <galaxys3tab32@gmail.com>
2026-03-09 23:34:21 -0300

    create blueprint

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from routes.user import user_bp
 
 app =Flask(__name__)
 
 @app.route("/")
 def main():
 	return "<h1>Ola mundo</h1>"
+
+app.register_blueprint(user_bp, url_prefix='/user')
 
 app.run(debug=True,port="4000")
